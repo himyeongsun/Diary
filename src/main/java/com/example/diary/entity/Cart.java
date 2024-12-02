eb3c71a7ac7707308c851a63c042ab2ec55e4caa
myeongsun <himyeongsun@gmail.com>
2024-12-02 16:59:58 +0900

    241202

--- src/main/java/com/example/diary/entity/Cart.java
+++ src/main/java/com/example/diary/entity/Cart.java
@@ -1,4 +1,25 @@
 package com.example.diary.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "cart")
 public class Cart {
+
+    @Id
+    @Column(name = "cart_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne   // 회원 앤티티와 일대일로 매핑
+    @JoinColumn(name = "member_id") // 매핑할 외래키 지정
+    private Member member;
+
 }
